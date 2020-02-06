80845130e54f3ee48a57d91a76be8dcdb967d669
Mackendito <60575267+Mackendito@users.noreply.github.com>
2020-02-06 18:05:48 -0500

    Hello World file

--- My first assignment.cpp
+++
@@ -1,3 +0,0 @@
-# Hello-World
-Just another repository for the fun of it. 
-My name is Mackendy Alcinvil. While I am new to this, I am very excited about learning and using this tool. I wish to learn as much as I can.
